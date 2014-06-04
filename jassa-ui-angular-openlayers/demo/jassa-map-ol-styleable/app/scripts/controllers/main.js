cbbec0a831bad9fdda96be26303f00bdb3361b17
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2014-06-04 23:19:12 +0200

    added demo project for styleable Jassa ol map

---
+++ jassa-ui-angular-openlayers/demo/jassa-map-ol-styleable/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jassaMapOlStyleableApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
